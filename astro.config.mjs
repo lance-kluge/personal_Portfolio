81749a7eaeb7a10a33c54a23c015ce56da2af4cf
Lance Kluge <lance.kluge@gmail.com>
2025-09-14 19:14:12 -0600

    remove assets prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   build: {
-    assetsPrefix: './',
+    //assetsPrefix: './',
   },
   vite: {
     plugins: [tailwindcss()],
