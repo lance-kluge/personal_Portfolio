453b0d3d179e71c4d3f19f1b255569557dc2876f
Lance Kluge <lance.kluge@gmail.com>
2025-09-14 18:26:50 -0600

    tries adding a base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lance-kluge.github.io',
+   base: '/personal_Portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
